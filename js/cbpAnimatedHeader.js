759a8aa8984079afa5f4209f5bf91f6c77620bda
RobertSabiryanov <robert.sabiryanov@gmail.com>
2016-05-25 23:21:14 +0500

    changes

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -26,13 +26,13 @@ var cbpAnimatedHeader = (function() {
 	}
 
 	function scrollPage() {
-		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove( header, 'navbar-shrink' );
-		}
+		//var sy = scrollY();
+		//if ( sy >= changeHeaderOn ) {
+		//	classie.add( header, 'navbar-shrink' );
+		//}
+		//else {
+		//	classie.remove( header, 'navbar-shrink' );
+		//}
 		didScroll = false;
 	}
 
